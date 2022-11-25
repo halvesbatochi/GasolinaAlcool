5a12a4477e1c69a03102d7acf1459128a961b29c
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2022-11-25 00:11:48 -0300

    Elementos da Home

--- GasolinaAlcool/Home/Screen/HomeScreen.swift
+++ GasolinaAlcool/Home/Screen/HomeScreen.swift
@@ -16,10 +16,32 @@ class HomeScreen: UIView {
         image.contentMode = .scaleAspectFit
         return image
     }()
+    
+    lazy var logoAppImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "LOGO")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Começar", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.backgroundColor = UIColor(red: 230/255, green: 0/255, blue: 127/255, alpha: 1.0)
+        return button
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(backgroundImageView)
+        addSubview(logoAppImageView)
+        addSubview(startButton)
         configConstraints()
     }
     
@@ -33,8 +55,16 @@ class HomeScreen: UIView {
             backgroundImageView.topAnchor.constraint(equalTo: topAnchor),
             backgroundImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
             backgroundImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
             
+            logoAppImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            logoAppImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            logoAppImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            startButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -125),
+            startButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            startButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            startButton.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
 
