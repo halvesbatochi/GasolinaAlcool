ce8fd0fbd882d4a72ed7eba2d45c52d51a4b2a0e
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2023-01-03 02:42:48 -0300

    Transferencia de valor entre telas

--- GasolinaAlcool/Calculator/CalculatorVC.swift
+++ GasolinaAlcool/Calculator/CalculatorVC.swift
@@ -55,16 +55,16 @@ extension CalculatorVC: CalculatorScreenDelegate {
             let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.0") as? Double) ?? 0.0
             let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.0") as? Double) ?? 0.0
             
-            let vc = ResultVC()
-            
-            
+            var vc: ResultVC?
             if ethanolPrice / gasPrice > 0.7 {
                 print("Melhor utilizar Gasolina!")
+                vc = ResultVC(bestFuel: .gas)
             } else {
                 print("Melhor utilizar Álcool!")
+                vc = ResultVC(bestFuel: .ethanol)
             }
             
-            navigationController?.pushViewController(vc, animated: true)
+            navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
         }
         
     }
