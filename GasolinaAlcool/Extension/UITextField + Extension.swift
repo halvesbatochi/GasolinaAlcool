728c2ae3343f782f96b3d33071bafb6bcfd31c4c
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2023-02-18 17:26:16 -0300

    Correção grafia

--- GasolinaAlcool/Extension/UITextField + Extension.swift
+++ GasolinaAlcool/Extension/UITextField + Extension.swift
@@ -12,16 +12,16 @@ extension UITextField {
     
     func addDoneButtonOnKeyboard() {
         
-        let doneToobar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
-        doneToobar.barStyle = .default
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
         
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let done = UIBarButtonItem(title: "OK", style: .done, target: self, action: #selector(doneButtonAction))
    
         let items = [flexSpace, done]
-        doneToobar.items = items
-        doneToobar.sizeToFit()
-        self.inputAccessoryView = doneToobar
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        self.inputAccessoryView = doneToolbar
     }
     
     @objc func doneButtonAction() {
